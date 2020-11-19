c8edeb424f2d848edadad793e0296dce802ec56e
SPQR14 <albertopicolara@gmail.com>
2020-11-18 18:04:38 -0600

    18/11/2020 - 15

--- Scripts/Windows/matrices.py
+++ Scripts/Windows/matrices.py
@@ -1,5 +1,7 @@
-import pandas as pd 
-df = pd.read_csv("D:\Desarrollo\Proyecto_16DeSeptiembre\Data_set\Chispazo.csv", sep = ',', encoding='utf8')
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("..\..\Data_set\Chispazo.csv", sep = ',', encoding='utf8')
 
 v1 = list(df['R1'])
 v2 = list(df['R2'])
@@ -18,26 +20,22 @@ def matriz_de_transicion(transiciones):
             r[:] = [f/s for f in r]
     return M
 
-m1 = matriz_de_transicion(v1)
-
-m2 = matriz_de_transicion(v2)
-
-m3 = matriz_de_transicion(v3)
-
-m4 = matriz_de_transicion(v4)
-
-m5 = matriz_de_transicion(v5)
+m1 = np.multiply(np.asmatrix(matriz_de_transicion(v1)).transpose(), 100.0)
+m2 = np.multiply(np.asmatrix(matriz_de_transicion(v2)).transpose(), 100.0)
+m3 = np.multiply(np.asmatrix(matriz_de_transicion(v3)).transpose(), 100.0)
+m4 = np.multiply(np.asmatrix(matriz_de_transicion(v4)).transpose(), 100.0)
+m5 = np.multiply(np.asmatrix(matriz_de_transicion(v5)).transpose(), 100.0)
 
 def pasar_a_csv(m, x):
     df = pd.DataFrame(m)
-    df.to_csv(f"D:\Desarrollo\Proyecto_16DeSeptiembre\Data_set\m{x}.csv", index = False, sep = ',', encoding = 'utf8')
+    df.to_csv(f"..\..\Data_set\m{x}.csv", index = False, sep = ',', encoding = 'utf8')
+
 
 pasar_a_csv(m1, 1)
 pasar_a_csv(m2, 2)
 pasar_a_csv(m3, 3)
 pasar_a_csv(m4, 4)
 pasar_a_csv(m5, 5)
-
 
 
 
